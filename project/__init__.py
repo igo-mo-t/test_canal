b48f6d0cadd7c761d2199ff5e2d37cb91ff2c0ab
IgorR <igorrozhnov95@gmail.com>
2023-03-28 09:39:03 +0300

    base path changed

--- project/__init__.py
+++ project/__init__.py
@@ -6,9 +6,9 @@ from flask_migrate import Migrate
 app=Flask(__name__)
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@db/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@db/test'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@127.0.0.1:5433/test'
+
 
 
 db=SQLAlchemy(app)
