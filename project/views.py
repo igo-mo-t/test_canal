62a9711d6ea6d2581bc82c871c13172efcf1174f
IgorR <igorrozhnov95@gmail.com>
2023-03-28 09:37:02 +0300

    views func added

--- project/views.py
+++ project/views.py
@@ -1,13 +1,16 @@
 from project import app
-from project.functions import *
+from project.functions import get_data_from_db_for_template, calculate_total_dollar, calculate_total_rub
 from flask import render_template
 
 
-# @app.route('/')
-# def home():
-#     table_data = [
-        
-#     ]
-#     return render_template('1.html', table_data=table_data)
+@app.route('/order_data')
+def order_data() -> str:
+    """
+    Возвращает HTML шаблон с данными по заказам.
+    """
+    return render_template('1.html', 
+                           table_data = get_data_from_db_for_template(), 
+                           total_dollar = calculate_total_dollar(), 
+                           total_rub = calculate_total_rub())
 
     
